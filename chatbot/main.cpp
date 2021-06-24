9ac77759de06ae72c861a612cf98b04f25b48f7c
vovapasko <w.pasko@neudologiq.de>
2021-06-24 11:50:43 +0200

    Initial Commit

---
+++ chatbot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chatbot
+//
+//  Created by Wladimir Pasko on 24.06.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
